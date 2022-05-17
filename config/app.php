0ee558b129333639c1121d0594b3a70afa734dd8
antonio0089 <jantoniovelasco97@gmail.com>
2022-05-17 09:47:37 -0500

    pdf

--- config/app.php
+++ config/app.php
@@ -176,6 +176,9 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        
+        Reliese\Coders\CodersServiceProvider::class,
+        Laravel\Socialite\SocialiteServiceProvider::class
 
     ],
 
@@ -192,6 +195,8 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // ...
+        'Socialite' => Laravel\Socialite\Facades\Socialite::class,
+
     ])->toArray(),
 
 ];
--- resources/views/candidato/create.blade.php
+++ resources/views/candidato/create.blade.php
@@ -19,8 +19,11 @@
             </ul>
         </div><br />
         @endif
-        <form method="post" action="{{ route('candidato.store') }} " 
-        enctype="multipart/form-data">
+        <form method="post" 
+            action="{{ route('candidato.store') }} " 
+            enctype="multipart/form-data"
+            onsubmit="return validateData();">
+            <!-- comment -->
             {{ csrf_field() }}
             <div class="form-group">
                 <label for="nombrecompleto">Nombre completo:</label>
@@ -37,16 +40,26 @@
             <div class="form-group">
                 <label for="foto">Foto:</label>
                 <input type="file" id="foto" accept="image/png, image/jpeg" 
-                 class="form-control" name="foto" />
+                 class="form-control" name="foto" 
+                 onchange="previewImage(event,'imageCandidato');"
+                  />
+                  <img src="" id="imageCandidato" width="200px" heigth="200px">
             </div>
             <div class="form-group">
                 <label for="perfil">Perfil:</label>
                 <input type="file" id="perfil" accept="application/pdf"
-                 class="form-control" name="perfil" />
+                class="form-control" name="perfil" onchange="return validarExt()"/>
             </div>
+            <br><br>
+			<div id="visorArchivo">
+				<!--Aqui se desplegará el fichero-->
+			</div>
 
-            <button type="submit" class="btn btn-primary">Guardar</button>
+            <button type="submit" 
+            class="btn btn-primary">Guardar</button>
         </form>
     </div>
 </div>
+<script type="text/javascript" src="{{ asset('js/custom.js') }}">
+</script>
 @endsection
